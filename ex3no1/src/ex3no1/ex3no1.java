1d2e5f8cd25557f37239394fcaab8b7b29e1b3f1
admin <admin@DESKTOP-8L2IG92>
2021-04-08 07:00:14 +0900

    ex3no1

---
+++ ex3no1/src/ex3no1/ex3no1.java
@@ -0,0 +1,14 @@
+package ex3no1;
+
+public class ex3no1 { //93p
+	public static void main(String[] args) {
+		int i = 5;
+		i++;
+		System.out.println(i);
+		
+		i=5;
+		++i;
+		System.out.println(i);
+	}
+
+}
